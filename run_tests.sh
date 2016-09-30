09ceeb3a8ff95f4be3bc17c9f05eacec0fbd394d
En Zhou <nathan.zhou@voss-solutions.com>
2016-09-30 11:11:20 +0200

    Removed checking on Python installation Removed executable permission from files

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,1 @@
-python -c "import sys"
-MSG=$?
-if [ "$MSG" == "0" ]; then
-    python test_stats.py  ./tests/test_hosts.txt ./tests/test_instances.txt
-else
-    echo "Worning: Python is NOT found in your system."
-fi
+python test_stats.py  ./tests/test_hosts.txt ./tests/test_instances.txt
